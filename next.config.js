8382a341c7e9fd23e9d93593b78679ab62c75929
Khalid-altalib <66210571+Khalid-altalib@users.noreply.github.com>
2024-06-21 17:43:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      // NextJS <Image> component needs to whitelist domains for src={}
       "lh3.googleusercontent.com",
       "pbs.twimg.com",
       "images.unsplash.com",
       "logos-world.net",
     ],
+  },
+  typescript: {
+    // Disable type checking during build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Disable ESLint warnings during build
+    ignoreDuringBuilds: true,
   },
 };
 
